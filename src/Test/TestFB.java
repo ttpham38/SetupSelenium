b2424b82f7266ec0082f57578a4140f0717f6390
Pham Thanh Tung <phamtung4312@gmail.com>
2023-11-03 19:36:26 +0700

    update user and password

--- src/Test/TestFB.java
+++ src/Test/TestFB.java
@@ -6,15 +6,15 @@ public class TestFB extends Init {
     public static void main(String[] args) throws Exception{
         Setup();
         driver.navigate().to("https://www.facebook.com/");
-        driver.findElement(By.name("email")).sendKeys("phamtung4312@gmail.com");
-        driver.findElement(By.name("pass")).sendKeys("@Angel09091997");
+        driver.findElement(By.name("email")).sendKeys("xxxxxxxxxxx@gmail.com");
+        driver.findElement(By.name("pass")).sendKeys("xxxxxxxxxxx");
         driver.findElement(By.name("login")).click();
         TearDown();
 
         Setup();
         driver.navigate().to("https://www.facebook.com/");
-        driver.findElement(By.name("email")).sendKeys("phamtung4312@gmail.com");
-        driver.findElement(By.name("pass")).sendKeys("@Angel091997");
+        driver.findElement(By.name("email")).sendKeys("xxxxxxxxxxx@gmail.com");
+        driver.findElement(By.name("pass")).sendKeys("xxxxxxxxxxx");
         driver.findElement(By.name("login")).click();
         TearDown();
     }
